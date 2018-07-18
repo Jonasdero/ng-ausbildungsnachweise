31e28f2c6a257e4b0eb29b9c860a37c9e1f1cb67
Jonasdero <jonas.roser@gmail.com>
2018-07-18 23:55:43 +0200

    Initial Word Input

---
+++ src/app/input-week/input-week.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-input-week',
+  templateUrl: './input-week.component.html',
+  styleUrls: ['./input-week.component.scss']
+})
+export class InputWeekComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/shared/department.ts
@@ -0,0 +1,5 @@
+export enum Department {
+  Atiw, 'Atiw Paderborn',
+  Spe, 'Siemens Professional Education Paderborn',
+  Praxis, 'Atos, AIS GER HR PD Azubi, Dr.-Mack-Straße 91, Fürth',
+}
