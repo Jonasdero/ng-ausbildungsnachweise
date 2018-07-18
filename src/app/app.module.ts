2bdc74c7c13d287fdeeb11c2d16b2f39f45a4f57
a642375 <jonas.roser@atos.net>
2018-07-18 15:38:52 +0200

    Initial Dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './shared/material.module';
+import { LayoutModule } from '@angular/cdk/layout';
+import {
+  MatButtonModule, MatCardModule, MatDialog, MatDialogModule,
+  MatIconModule, MatFormFieldModule, MatInputModule, MatListModule,
+  MatSidenavModule, MatToolbarModule,
+} from '@angular/material';
+
+// Components
+import { AppComponent } from './app.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialModule,
+
+    // Angular Material
+    LayoutModule,
+    MatButtonModule,
+    MatCardModule,
+    MatDialogModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatListModule,
+    MatSidenavModule,
+    MatToolbarModule,
   ],
-  providers: [],
+  providers: [MatDialog],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
