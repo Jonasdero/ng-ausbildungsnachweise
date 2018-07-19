d6e1737d6b49834e1ef05e316236a965e55478e4
a642375 <jonas.roser@atos.net>
2018-07-19 13:42:48 +0200

    Content input

--- src/app/input-week/input-week.component.ts
+++ src/app/input-week/input-week.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, FormControl, AbstractControl } from '@angular/forms';
 import { WeekService } from '../shared/week.service';
 
+function validateContent(c: AbstractControl) {
+  return c.value.split('\n').length <= 8 ? null : { length: c.value.split('\n').length };
+}
+
 @Component({
   selector: 'input-week',
   templateUrl: './input-week.component.html',
@@ -31,11 +35,11 @@ export class InputWeekComponent implements OnInit {
       'hMi': new FormControl(this.week.hMi, Validators.required),
       'hDo': new FormControl(this.week.hDo, Validators.required),
       'hFr': new FormControl(this.week.hFr, Validators.required),
-      'contentMo': new FormControl(this.mergeContent('Mo'), Validators.required),
-      'contentDi': new FormControl(this.mergeContent('Di'), Validators.required),
-      'contentMi': new FormControl(this.mergeContent('Mi'), Validators.required),
-      'contentDo': new FormControl(this.mergeContent('Do'), Validators.required),
-      'contentFr': new FormControl(this.mergeContent('Fr'), Validators.required),
+      'contentMo': new FormControl(this.mergeContent('Mo'), [Validators.required, validateContent]),
+      'contentDi': new FormControl(this.mergeContent('Di'), [Validators.required, validateContent]),
+      'contentMi': new FormControl(this.mergeContent('Mi'), [Validators.required, validateContent]),
+      'contentDo': new FormControl(this.mergeContent('Do'), [Validators.required, validateContent]),
+      'contentFr': new FormControl(this.mergeContent('Fr'), [Validators.required, validateContent]),
     })
 
     this.form.valueChanges.subscribe((data) => {
@@ -54,14 +58,18 @@ export class InputWeekComponent implements OnInit {
   mergeContent(day: string) {
     let result: string = '';
     for (let i = 1; i <= 8; i++) {
-      if (i == 8) result += this.week['content' + day + i];
-      else result += this.week['content' + day + i] + '\n';
+      if (this.week['content' + day + i].trim().length > 0)
+        if (i == 8) result += this.week['content' + day + i];
+        else result += this.week['content' + day + i] + '\n';
     }
     return result;
   }
 
   splitContent(name: string, content: string) {
     let splitted = content.split('\n');
+    while (splitted.length < 8) {
+      splitted.push('');
+    }
     for (let i = 1; i <= 8; i++)
       this.week[name + i] = splitted[i - 1];
   }
