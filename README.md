0866d3a8c493b56d9b75d3795b1ef4ce1654fd97
Jonasdero <jonas.roser@gmail.com>
2018-07-19 19:10:59 +0200

    Added Help site

--- README.md
+++ README.md
@@ -1,27 +1,20 @@
-# NgAusbildungsnachweise
+# Ausbildungsnachweisgenerator
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.0.8.
+[![forthebadge](https://forthebadge.com/images/badges/you-didnt-ask-for-this.svg)](https://ng-ausbildungsnachweise.firebaseapp.com/)
+[![forthebadge](https://forthebadge.com/images/badges/fuck-it-ship-it.svg)](https://ng-ausbildungsnachweise.firebaseapp.com/)
+## Funktionen
 
-## Development server
+- Ändern von Vor- und Nachnamen, Beruf, Ausbildungsstartdaum
+- Einstellen von beliebigen Abteilungsnamen
+- Hinzufügen von Ausbildungswochen mit Startdatum, Stunden pro Tag und Tätigkeiten pro Tag
+- Speichern aller Ausbildungswoche nach Schema `0NR_dd.MM.yyyy.docx`
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+## Todo
 
-## Code scaffolding
+- Import einer JSON Datei zur Batch-Worderstellung
 
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
+## Fragen, Anmerkungen oder Fehler?
 
-## Build
+- Aussagekräftiges Issue erstellen
 
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
-
-## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+[![forthebadge](https://forthebadge.com/images/badges/built-with-love.svg)](https://github.com/Jonasdero) by Jonasdero
--- src/app/input-word/input-word.component.ts
+++ src/app/input-word/input-word.component.ts
@@ -11,7 +11,7 @@ import { DateService } from '../shared/services/date.service';
 export class InputWordComponent implements OnInit {
   weeks: Week[] = [];
   departments: string[] = [];
-  constructor(private weekService: WeekService, private settingsService: SettingsService,
+  constructor(private weekService: WeekService, public settingsService: SettingsService,
     private wordService: WordService, private dateService: DateService) { }
 
   ngOnInit() {
@@ -28,20 +28,11 @@ export class InputWordComponent implements OnInit {
       startDate: '', endDate: '',
       date: this.dateService.getMonday(new Date),
       hMo: 7.5, hDi: 7.5, hMi: 7.5, hDo: 7.5, hFr: 7.5,
-      contentMo1: '', contentMo2: '', contentMo3: '', contentMo4: '',
-      contentMo5: '', contentMo6: '', contentMo7: '', contentMo8: '',
-
-      contentDi1: '', contentDi2: '', contentDi3: '', contentDi4: '',
-      contentDi5: '', contentDi6: '', contentDi7: '', contentDi8: '',
-
-      contentMi1: '', contentMi2: '', contentMi3: '', contentMi4: '',
-      contentMi5: '', contentMi6: '', contentMi7: '', contentMi8: '',
-
-      contentDo1: '', contentDo2: '', contentDo3: '', contentDo4: '',
-      contentDo5: '', contentDo6: '', contentDo7: '', contentDo8: '',
-
-      contentFr1: '', contentFr2: '', contentFr3: '', contentFr4: '',
-      contentFr5: '', contentFr6: '', contentFr7: '', contentFr8: '',
+      contentMo1: '', contentMo2: '', contentMo3: '', contentMo4: '', contentMo5: '', contentMo6: '', contentMo7: '', contentMo8: '',
+      contentDi1: '', contentDi2: '', contentDi3: '', contentDi4: '', contentDi5: '', contentDi6: '', contentDi7: '', contentDi8: '',
+      contentMi1: '', contentMi2: '', contentMi3: '', contentMi4: '', contentMi5: '', contentMi6: '', contentMi7: '', contentMi8: '',
+      contentDo1: '', contentDo2: '', contentDo3: '', contentDo4: '', contentDo5: '', contentDo6: '', contentDo7: '', contentDo8: '',
+      contentFr1: '', contentFr2: '', contentFr3: '', contentFr4: '', contentFr5: '', contentFr6: '', contentFr7: '', contentFr8: '',
     })
     this.getWeeks();
   }
