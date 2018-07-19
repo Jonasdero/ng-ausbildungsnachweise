95139bcc12f7483d67ff672a29da1f3bc30b9877
Jonasdero <jonas.roser@gmail.com>
2018-07-19 18:05:11 +0200

    Reorganized Material Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,37 +9,15 @@ import { HttpClientModule } from '@angular/common/http';
 import { AngularFireModule } from 'angularfire2';
 import { environment } from '../environments/environment';
 
-// Angular Material
-import {
-  MatDialog,
-  MatButtonModule, MatCardModule, MatDialogModule, MatIconModule, MatSelectModule,
-  MatFormFieldModule, MatInputModule, MatListModule, MatSidenavModule,
-  MatToolbarModule, MatGridListModule, MatDatepickerModule, MatNativeDateModule, DateAdapter, MAT_DATE_FORMATS
-} from '@angular/material';
-
-// Classes
-import { GermanDateAdapter } from './shared/GermanDateAdapter';
-
 // Modules
 import { AppRoutingModule } from './app-routing.module';
+import { MaterialModule } from './shared/material.module';
 
 // Components
 import { AppComponent } from './app.component';
 import { InputWordComponent } from './input-word/input-word.component';
 import { InputWeekComponent } from './input-week/input-week.component';
 import { SettingsComponent } from './settings/settings.component';
-
-
-// Const
-const MY_DATE_FORMATS = {
-  parse: { dateInput: { month: 'short', year: 'numeric', day: 'numeric' } },
-  display: {
-    dateInput: 'input',
-    monthYearLabel: { year: 'numeric', month: 'short' },
-    dateA11yLabel: { year: 'numeric', month: 'long', day: 'numeric' },
-    monthYearA11yLabel: { year: 'numeric', month: 'long' },
-  }
-};
 
 @NgModule({
   declarations: [
@@ -58,15 +36,9 @@ const MY_DATE_FORMATS = {
 
     // Angular Material
     LayoutModule,
-    MatButtonModule, MatCardModule, MatDialogModule, MatIconModule, MatSelectModule,
-    MatFormFieldModule, MatInputModule, MatListModule, MatSidenavModule,
-    MatToolbarModule, MatGridListModule, MatDatepickerModule, MatNativeDateModule,
+    MaterialModule,
   ],
-  providers: [
-    MatDialog,
-    { provide: DateAdapter, useClass: GermanDateAdapter },
-    { provide: MAT_DATE_FORMATS, useValue: MY_DATE_FORMATS },
-  ],
+  providers: [    ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/shared/material.module.ts
@@ -0,0 +1,54 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  MatDialog, NativeDateAdapter, DateAdapter, MAT_DATE_FORMATS,
+  MatButtonModule, MatCardModule, MatDialogModule, MatIconModule, MatSelectModule,
+  MatFormFieldModule, MatInputModule, MatListModule, MatSidenavModule, MatExpansionModule,
+  MatToolbarModule, MatGridListModule, MatDatepickerModule, MatNativeDateModule,
+} from '@angular/material';
+
+const MY_DATE_FORMATS = {
+  parse: { dateInput: { month: 'short', year: 'numeric', day: 'numeric' } },
+  display: {
+    dateInput: 'input',
+    monthYearLabel: { year: 'numeric', month: 'short' },
+    dateA11yLabel: { year: 'numeric', month: 'long', day: 'numeric' },
+    monthYearA11yLabel: { year: 'numeric', month: 'long' },
+  }
+};
+
+class GermanDateAdapter extends NativeDateAdapter {
+  format(date: Date, displayFormat: Object): string {
+    if (displayFormat != "input")
+      return date.toDateString();
+
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+    return this._to2digit(day) + '.' + this._to2digit(month) + '.' + year;
+  }
+
+  private _to2digit(n: number) {
+    return ('00' + n).slice(-2);
+  }
+}
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MatButtonModule, MatCardModule, MatDialogModule, MatIconModule, MatSelectModule,
+    MatFormFieldModule, MatInputModule, MatListModule, MatSidenavModule, MatExpansionModule,
+    MatToolbarModule, MatGridListModule, MatDatepickerModule, MatNativeDateModule
+  ],
+  exports: [
+    MatButtonModule, MatCardModule, MatDialogModule, MatIconModule, MatSelectModule,
+    MatFormFieldModule, MatInputModule, MatListModule, MatSidenavModule, MatExpansionModule,
+    MatToolbarModule, MatGridListModule, MatDatepickerModule, MatNativeDateModule
+  ],
+  providers: [
+    MatDialog,
+    { provide: DateAdapter, useClass: GermanDateAdapter },
+    { provide: MAT_DATE_FORMATS, useValue: MY_DATE_FORMATS }
+  ]
+})
+export class MaterialModule { }
