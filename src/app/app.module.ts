0b507d62c87d5267a18f29ae1005ed802ac41b06
Jonasdero <jonas.roser@gmail.com>
2018-07-19 17:54:03 +0200

    Custom Date Format Material Datepicker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LayoutModule } from '@angular/cdk/layout';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+
+// Firebase
 import { AngularFireModule } from 'angularfire2';
 import { environment } from '../environments/environment';
+
+// Angular Material
 import {
   MatDialog,
-  MatButtonModule, MatCardModule, MatDialogModule, MatIconModule,
+  MatButtonModule, MatCardModule, MatDialogModule, MatIconModule, MatSelectModule,
   MatFormFieldModule, MatInputModule, MatListModule, MatSidenavModule,
-  MatToolbarModule, MatGridListModule, MatDatepickerModule, MatNativeDateModule,
-  MatSelectModule
+  MatToolbarModule, MatGridListModule, MatDatepickerModule, MatNativeDateModule, DateAdapter, MAT_DATE_FORMATS
 } from '@angular/material';
+
+// Classes
+import { GermanDateAdapter } from './shared/GermanDateAdapter';
 
 // Modules
 import { AppRoutingModule } from './app-routing.module';
@@ -23,31 +29,43 @@ import { InputWordComponent } from './input-word/input-word.component';
 import { InputWeekComponent } from './input-week/input-week.component';
 import { SettingsComponent } from './settings/settings.component';
 
+
+// Const
+const MY_DATE_FORMATS = {
+  parse: { dateInput: { month: 'short', year: 'numeric', day: 'numeric' } },
+  display: {
+    dateInput: 'input',
+    monthYearLabel: { year: 'numeric', month: 'short' },
+    dateA11yLabel: { year: 'numeric', month: 'long', day: 'numeric' },
+    monthYearA11yLabel: { year: 'numeric', month: 'long' },
+  }
+};
+
 @NgModule({
   declarations: [
     AppComponent,
-    InputWordComponent,
-    InputWeekComponent,
+    InputWordComponent, InputWeekComponent,
     SettingsComponent
   ],
   imports: [
     AppRoutingModule,
-    BrowserModule,
-    BrowserAnimationsModule,
+    BrowserModule, BrowserAnimationsModule,
     HttpClientModule,
     FormsModule, ReactiveFormsModule,
 
+    // Firebase
     AngularFireModule.initializeApp(environment.firebase),
 
     // Angular Material
     LayoutModule,
-    MatButtonModule, MatCardModule, MatDialogModule, MatIconModule,
+    MatButtonModule, MatCardModule, MatDialogModule, MatIconModule, MatSelectModule,
     MatFormFieldModule, MatInputModule, MatListModule, MatSidenavModule,
     MatToolbarModule, MatGridListModule, MatDatepickerModule, MatNativeDateModule,
-    MatSelectModule
   ],
   providers: [
     MatDialog,
+    { provide: DateAdapter, useClass: GermanDateAdapter },
+    { provide: MAT_DATE_FORMATS, useValue: MY_DATE_FORMATS },
   ],
   bootstrap: [AppComponent]
 })
