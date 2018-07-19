4499fd2c633da3d9b87c102185cb2f0359e2b628
Jonasdero <jonas.roser@gmail.com>
2018-07-19 14:09:15 +0200

    Version 1.0.0 Basic Word generation

--- src/app/shared/settings.service.ts
+++ src/app/shared/settings.service.ts
@@ -6,11 +6,15 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class SettingsService {
+  settings: Settings;
   path = 'assets/settings.json';
   httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) }
   constructor(private http: HttpClient) { }
 
   getSettings(): Observable<Settings> {
+    this.http.get<Settings>(this.path).subscribe((s) => {
+      this.settings = s;
+    });
     return this.http.get<Settings>(this.path);
   }
 
@@ -23,9 +27,5 @@ export class SettingsService {
     document.body.appendChild(element);
     element.click(); // simulate click
     document.body.removeChild(element);
-  }
-
-  getAusbildungsjahr(d: Date): number {
-    return 1;
   }
 }
